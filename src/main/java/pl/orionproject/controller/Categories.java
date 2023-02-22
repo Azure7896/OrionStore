89560a58f3f1463d6aa8b54743dc48b5084835bd
Azure7896 <azurusek@gmail.com>
2023-02-22 13:09:37 +0100

    Connected categories list to the categories from database in homepage 02.22

--- src/main/java/pl/orionproject/controller/Categories.java
+++ src/main/java/pl/orionproject/controller/Categories.java
@@ -6,20 +6,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import pl.orionproject.model.Item;
+import pl.orionproject.repository.CategoryRepository;
 import pl.orionproject.repository.ItemRepository;
 
 @Controller
-public class CategoryController {
+public class Categories {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    CategoryRepository categoryRepository;
 
     @GetMapping("/category/{id}")
     public String registerUserAccount(@ModelAttribute("items") Item item, Model model,
                                       @PathVariable Long id) {
 //        model.addAttribute("items", itemRepository.findItemByCategory()
         return "home";
+    }
+
+    @GetMapping("/categories")
+    public String showCategoriesList(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "categories";
     }
 }
