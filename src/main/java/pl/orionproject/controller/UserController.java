f8e864f2391233f63ed2e004321263b3359cc280
Azure7896 <azurusek@gmail.com>
2023-02-25 02:38:58 +0100

    Email verification finished 02.25

--- src/main/java/pl/orionproject/controller/UserController.java
+++ src/main/java/pl/orionproject/controller/UserController.java
@@ -5,13 +5,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.orionproject.DataTransferObjects.UserRegistrationDto;
+import pl.orionproject.model.ConfirmationToken;
+import pl.orionproject.model.User;
+import pl.orionproject.repository.ConfirmationTokenRepository;
+import pl.orionproject.repository.UserRepository;
 import pl.orionproject.service.UserService;
 
 
 @Controller
 public class UserController {
 
+    @Autowired
+    ConfirmationTokenRepository confirmationTokenRepository;
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     private UserValidator userValidator;
     @Autowired
@@ -51,6 +60,20 @@ public class UserController {
     }
 
 
+    @GetMapping("/confirm-account")
+    public String confirmAccount(@RequestParam("token") String confirmToken) {
+        ConfirmationToken token = confirmationTokenRepository.findByConfirmationToken(confirmToken);
+        if (token != null) {
+            User user = userRepository.findByEmail(token.getUser().getEmail());
+            user.setEnabled(true);
+            userRepository.save(user);
+            confirmationTokenRepository.delete(token);
+            return "registrationsuccesful";
+        } else {
+            return "registerfail";
+        }
+
+    }
 }
 
 
