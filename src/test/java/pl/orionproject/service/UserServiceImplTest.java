f3776f5601e7d7dbb4700e5bf1aac10e1791bba8
Azure7896 <azurusek@gmail.com>
2023-03-11 02:17:42 +0100

    UserService test class added 03.11

---
+++ src/test/java/pl/orionproject/service/UserServiceImplTest.java
@@ -0,0 +1,40 @@
+package pl.orionproject.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @Test
+    void registerUser() {
+    }
+
+    @Test
+    void activateAccount() {
+    }
+
+    @Test
+    void addUserAdditionalInformation() {
+    }
+
+    @Test
+    void loadUserByUsername() {
+    }
+
+    @Test
+    void getAuthorities() {
+    }
+
+    @Test
+    void createHelloNotification() {
+    }
+
+    @Test
+    void getUserSessionEmailName() {
+    }
+
+    @Test
+    void getUserFromDatabaseBySession() {
+    }
+}
